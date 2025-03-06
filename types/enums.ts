8a84c3a6b7e9b0fc7e7ecff3436763ffd9651f11
Carter Rollins <carter.rollins@noaa.gov>
2025-03-06 02:13:50 -0500

    Updates dead values

--- types/enums.ts
+++ types/enums.ts
@@ -24,10 +24,18 @@ import {
 } from "@/prisma/generated/zod";
 
 export enum DeadValueEnum {
-	"not applicable" = -9999,
-	"not collected",
-	"not provided",
-	"missing"
+	"not applicable: control sample" = -9999,
+	"not applicable: sample group",
+	"not applicable",
+	"missing: not collected: synthetic construct",
+	"missing: not collected: lab stock",
+	"missing: not collected: third party data",
+	"missing: not collected",
+	"missing: not provided: data agreement established pre-2023",
+	"missing: not provided",
+	"missing: restricted access: endangered species",
+	"missing: restricted access: human-identifiable",
+	"missing: restricted access"
 }
 
 export const DeadBooleanEnum = {
@@ -35,10 +43,19 @@ export const DeadBooleanEnum = {
 	"0": "false",
 	true: "true",
 	"1": "true",
+	"not applicable: control sample": "not_applicableCOLON__control_sample",
+	"not applicable: sample group": "not_applicableCOLON__sample_group",
 	"not applicable": "not_applicable",
-	"not collected": "not_collected",
-	"not provided": "not_provided",
-	missing: "missing"
+	"missing: not collected: synthetic construct": "missingCOLON__not_collectedCOLON__synthetic_construct",
+	"missing: not collected: lab stock": "missingCOLON__not_collectedCOLON__lab_stock",
+	"missing: not collected: third party data": "missingCOLON__not_collectedCOLON__third_party_data",
+	"missing: not collected": "missingCOLON__not_collected",
+	"missing: not provided: data agreement established pre-2023":
+		"missingCOLON__not_providedCOLON__data_agreement_established_pre__2023",
+	"missing: not provided": "missingCOLON__not_provided",
+	"missing: restricted access: endangered species": "missingCOLON__restricted_accessCOLON__endangered_species",
+	"missing: restricted access: human-identifiable": "missingCOLON__restricted_accessCOLON__human__identifiable",
+	"missing: restricted access": "missingCOLON__restricted_access"
 };
 
 export const TableToEnumSchema = {
