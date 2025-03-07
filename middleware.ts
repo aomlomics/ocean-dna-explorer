7bcb5fb1d209b61d70a975f48f9f44db97901cc1
Carter Rollins <carter.rollins@noaa.gov>
2025-03-07 14:16:36 -0500

    Adds sitemap and robots generator

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/admin(.*)", "/api/admin(.*)", "/submit(.*)", "/mySubmissions(.*)"]);
+const isProtectedRoute = createRouteMatcher(["/submit(.*)", "/mySubmissions(.*)"]);
 
 export default clerkMiddleware(async (auth, req) => {
 	if (isProtectedRoute(req)) await auth.protect();
