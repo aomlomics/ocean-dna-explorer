5ed0b2b69823ea985ee649bdccdf633afe17b8e2
Carter Rollins <carter.rollins@noaa.gov>
2025-03-27 10:24:50 -0400

    Fixes siteUrl

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-	siteUrl: process.env.SITE_URL,
+	siteUrl: process.env.NEXT_PUBLIC_URL,
 	exclude: ["/sign-in", "/mySubmissions", "/submit/*", "/api/analysisFile/*", "/api/pagination/*", "/api/sampleLocations"],
 	generateRobotsTxt: true,
 	robotsTxtOptions: {
