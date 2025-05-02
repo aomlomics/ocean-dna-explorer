d374b4c7cd03526d44271f2561917178daeeab72
Carter Rollins <carter.rollins@noaa.gov>
2025-05-02 19:10:15 -0400

    Adds dynamic building of sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,65 +1,13 @@
 /** @type {import('next-sitemap').IConfig} */
 
-import { prisma } from "./app/helpers/prisma";
-
 module.exports = {
 	siteUrl: process.env.NEXT_PUBLIC_URL,
-	exclude: ["/sign-in/*", "/mySubmissions/*", "/submit/*", "/api/*", "/admin/*"],
-	additionalPaths: async () => {
-		const paths = [];
-
-		//projects
-		const projects = await prisma.project.findMany({
-			select: {
-				project_id: true
-			}
-		});
-		paths.push(projects.map((proj) => ({ loc: `/explore/project/${proj.project_id}` })));
-
-		//samples
-		const samples = await prisma.sample.findMany({
-			select: {
-				samp_name: true
-			}
-		});
-		paths.push(samples.map((samp) => ({ loc: `/explore/sample/${samp.samp_name}` })));
-
-		//assays
-		const assays = await prisma.assay.findMany({
-			select: {
-				assay_name: true
-			}
-		});
-		paths.push(assays.map((a) => ({ loc: `/explore/assay/${a.assay_name}` })));
-
-		//analyses
-		const analyses = await prisma.analysis.findMany({
-			select: {
-				analysis_run_name: true
-			}
-		});
-		paths.push(analyses.map((a) => ({ loc: `/explore/analysis/${a.analysis_run_name}` })));
-
-		//features
-		const features = await prisma.feature.findMany({
-			select: {
-				featureid: true
-			}
-		});
-		paths.push(features.map((feat) => ({ loc: `/explore/feature/${feat.featureid}` })));
-
-		//taxonomies
-		const taxonomies = await prisma.taxonomy.findMany({
-			select: {
-				taxonomy: true
-			}
-		});
-		paths.push(taxonomies.map((taxa) => ({ loc: `/explore/taxonomy/${taxa.taxonomy}` })));
-
-		return paths;
-	},
+	exclude: ["/server-sitemap.xml", "/sign-in/*", "/mySubmissions/*", "/submit/*", "/api/*", "/admin/*"],
 	generateRobotsTxt: true,
 	robotsTxtOptions: {
+		additionalSitemaps: [
+			`${process.env.NEXT_PUBLIC_URL}/server-sitemap.xml`
+		],
 		policies: [
 			{
 				userAgent: "*",
