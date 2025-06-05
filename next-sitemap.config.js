120b62900ae985299a4cc20a02cfe354a9ecb9d8
Carter Rollins <carter.rollins@noaa.gov>
2025-06-05 13:54:37 -0400

    Test

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,5 +3,5 @@
 module.exports = {
 	siteUrl: process.env.NEXT_PUBLIC_URL,
 	outDir: "app",
-	exclude: ["/sign-in/*", "/mySubmissions/*", "/submit/*", "/api/*", "/admin/*", "/**/sitemap.ts"]
+	exclude: ["/sign-in/*", "/mySubmissions/*", "/submit/*", "/api/*", "/admin/*", "/**/sitemap/**/*"]
 }
