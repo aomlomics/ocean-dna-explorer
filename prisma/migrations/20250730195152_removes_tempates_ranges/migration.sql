6bfbef9533c15d490fb9cedaddcd2fe7761c5c73
Carter Rollins <carter.rollins@noaa.gov>
2025-07-30 16:21:37 -0400

    Drops old columns before renaming

--- prisma/migrations/20250730195152_removes_tempates_ranges/migration.sql
+++ prisma/migrations/20250730195152_removes_tempates_ranges/migration.sql
@@ -1,10 +1,18 @@
 -- AlterTable
-ALTER TABLE "Analysis" RENAME COLUMN "percent_match__TEMP" TO "percent_match";
-ALTER TABLE "Analysis" RENAME COLUMN "percent_query_cover__TEMP" TO "percent_query_cover";
+ALTER TABLE "Analysis"
+DROP COLUMN "percent_match",
+RENAME COLUMN "percent_match__TEMP" TO "percent_match",
+DROP COLUMN "percent_query_cover",
+RENAME COLUMN "percent_query_cover__TEMP" TO "percent_query_cover";
 
 -- AlterTable
-ALTER TABLE "Assay" RENAME COLUMN "ampliconSize__TEMP" TO "ampliconSize";
+ALTER TABLE "Assay"
+DROP COLUMN "ampliconSize",
+RENAME COLUMN "ampliconSize__TEMP" TO "ampliconSize";
 
 -- AlterTable
-ALTER TABLE "Sample" RENAME COLUMN "eventDate__TEMP" TO "eventDate";
-ALTER TABLE "Sample" RENAME COLUMN "date_ext_TEMP" TO "date_ext";
+ALTER TABLE "Sample"
+DROP COLUMN "eventDate",
+RENAME COLUMN "eventDate__TEMP" TO "eventDate",
+DROP COLUMN "date_ext",
+RENAME COLUMN "date_ext_TEMP" TO "date_ext";
