d01a2e1cafe8d355a5bb6be25957da5a68c1a16f
Carter Rollins <carter.rollins@noaa.gov>
2025-07-30 16:19:29 -0400

    Makes each column rename it's own alter table

--- prisma/migrations/20250730195152_removes_tempates_ranges/migration.sql
+++ prisma/migrations/20250730195152_removes_tempates_ranges/migration.sql
@@ -1,10 +1,10 @@
 -- AlterTable
-ALTER TABLE "Analysis" RENAME COLUMN "percent_match__TEMP" TO "percent_match",
-RENAME COLUMN "percent_query_cover__TEMP" TO "percent_query_cover";
+ALTER TABLE "Analysis" RENAME COLUMN "percent_match__TEMP" TO "percent_match";
+ALTER TABLE "Analysis" RENAME COLUMN "percent_query_cover__TEMP" TO "percent_query_cover";
 
 -- AlterTable
 ALTER TABLE "Assay" RENAME COLUMN "ampliconSize__TEMP" TO "ampliconSize";
 
 -- AlterTable
-ALTER TABLE "Sample" RENAME COLUMN "eventDate__TEMP" TO "eventDate",
-RENAME COLUMN "date_ext_TEMP" TO "date_ext";
+ALTER TABLE "Sample" RENAME COLUMN "eventDate__TEMP" TO "eventDate";
+ALTER TABLE "Sample" RENAME COLUMN "date_ext_TEMP" TO "date_ext";
