6d8708d61362fd80bcc0ce634571fdd423b29b22
Carter Rollins <carter.rollins@noaa.gov>
2025-07-30 17:37:42 -0400

    Removes saving end from range arrays (data loss possible, but no data like that exists)

--- app/helpers/prisma.ts
+++ app/helpers/prisma.ts
@@ -611,12 +611,14 @@ export async function updateManyRaw(
 
 	const sql = `UPDATE "${table}" SET ${setSql} FROM (VALUES ${valuesSql}) AS t("${id}", ${fieldsSql}) WHERE "${table}"."${id}" = "t"."${id}"`;
 
-	const reduced = data.reduce(
-		(acc: Array<string | number | boolean>, row) => [...acc, row[id], ...fs.map((f) => row[f])],
-		[]
+	console.log(
+		sql,
+		JSON.stringify(
+			data.reduce((acc: Array<string | number | boolean>, row) => [...acc, row[id], ...fs.map((f) => row[f])], []),
+			undefined,
+			2
+		)
 	);
-	console.log(paramIndex, reduced.length);
-	console.log(sql, JSON.stringify(reduced, undefined, 2));
 	return client.$executeRawUnsafe(
 		sql,
 		...data.reduce((acc: Array<string | number | boolean>, row) => [...acc, row[id], ...fs.map((f) => row[f])], [])
