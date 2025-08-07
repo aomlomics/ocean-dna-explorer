92bd556fd9e0c7164912edaac0f073ec6ed67d36
Carter Rollins <carter.rollins@noaa.gov>
2025-08-07 17:02:59 -0400

    Fixes queries to properly merge together in extensions

--- app/helpers/utils.ts
+++ app/helpers/utils.ts
@@ -161,3 +161,25 @@ export function getOptions(arr: Record<string, any>[]) {
 
 	return filterOptions;
 }
+
+function isObject(item: any) {
+	return item && typeof item === "object" && !Array.isArray(item);
+}
+
+export function deepMerge(target: Record<string, any>, ...sources: Record<string, any>[]) {
+	if (!sources.length) return target;
+	const source = sources.shift();
+
+	if (isObject(target) && isObject(source)) {
+		for (const key in source) {
+			if (isObject(source[key])) {
+				if (!target[key]) Object.assign(target, { [key]: {} });
+				deepMerge(target[key], source[key]);
+			} else {
+				Object.assign(target, { [key]: source[key] });
+			}
+		}
+	}
+
+	return deepMerge(target, ...sources);
+}
