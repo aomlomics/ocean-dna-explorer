f636fb8305c6a9807356e1fc4613b93335fab02e
Bayden <bwillms9@gmail.com>
2025-11-05 15:08:02 -0500

    Switches loader when website loads to the new loader page

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,7 +1,5 @@
+import LoadingSpinner from "@/app/components/LoadingSpinner";
+
 export default function Loading() {
-	return (
-		<div className="min-h-screen flex items-center justify-center">
-			<span className="loading loading-spinner loading-xl text-primary"></span>
-		</div>
-	);
+	return <LoadingSpinner />;
 }
