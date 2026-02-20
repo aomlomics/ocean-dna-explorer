da3c40825976f73bceef8a7efa02cf88a504d01f
Carter Rollins <carter.rollins@noaa.gov>
2026-02-20 15:34:13 -0500

    Adds tabs to visualize

---
+++ app/components/VisualizeTabs.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname, useSearchParams } from "next/navigation";
+
+export default function VisualizeTabs() {
+	const pathname = usePathname();
+	const searchParams = useSearchParams();
+
+	return (
+		<nav className="flex">
+			<Link
+				href={`/visualize/metadata?${searchParams.toString()}`}
+				className={`btn px-6 py-3 transition-colors rounded-none ${
+					pathname === "/visualize/metadata" ? "rounded-t-lg btn-primary" : ""
+				}`}
+			>
+				Metadata
+			</Link>
+			<Link
+				href={`/visualize/taxonomy?${searchParams.toString()}`}
+				className={`btn px-6 py-3 transition-colors rounded-none ${
+					pathname === "/visualize/taxonomy" ? "rounded-t-lg btn-primary" : ""
+				}`}
+			>
+				Taxonomy
+			</Link>
+		</nav>
+	);
+}
