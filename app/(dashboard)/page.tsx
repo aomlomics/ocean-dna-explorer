d2453692f35008f1d140870c35020c48ad8c07c1
Carter Rollins <carter.rollins@noaa.gov>
2026-03-18 14:22:03 -0400

    Removes async from home

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -9,7 +9,7 @@ import Map from "@/app/components/map/Map";
 import { Suspense } from "react";
 import TopTaxonomiesSummary from "@/app/components/TopTaxonomiesSummary";
 
-export default async function Home() {
+export default function Home() {
 	return (
 		<main className="relative flex flex-col grow bg-base-400 text-base-content">
 			<div className="absolute top-0 left-0 right-0 z-50 bg-orange-500 text-white p-2 sm:p-4 text-center">
