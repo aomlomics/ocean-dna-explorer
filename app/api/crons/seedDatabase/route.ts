ab13ac77b919b01cf95b7b565332165ff0399119
Carter Rollins <carter.rollins@noaa.gov>
2026-03-20 22:44:11 -0400

    Replaces postbuild with cron to seed database once a month

---
+++ app/api/crons/seedDatabase/route.ts
@@ -0,0 +1,24 @@
+import { handlePrismaError, seedAssays } from "@/app/helpers/prisma";
+import { NetworkPacket } from "@/types/globals";
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request): Promise<NextResponse<NetworkPacket>> {
+	const authHeader = request.headers.get("authorization");
+	if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+		return NextResponse.json({ statusMessage: "error", error: "Unauthorized" });
+	}
+
+	try {
+		await seedAssays();
+
+		return NextResponse.json({ statusMessage: "success" });
+	} catch (err: any) {
+		const prismaErr = handlePrismaError(err);
+		if (prismaErr) {
+			return NextResponse.json(prismaErr);
+		}
+
+		const error = err as Error;
+		return NextResponse.json({ statusMessage: "error", error: error.message });
+	}
+}
