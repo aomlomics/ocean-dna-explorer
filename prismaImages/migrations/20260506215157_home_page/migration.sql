2559bb2ab8bc1e53e491a47afd189339c64aad31
CarterRoll <carter.rollins@noaa.gov>
2026-05-11 14:18:50 -0400

    Updates old migration to have default

--- prismaImages/migrations/20260506215157_home_page/migration.sql
+++ prismaImages/migrations/20260506215157_home_page/migration.sql
@@ -1,8 +1,2 @@
-/*
-  Warnings:
-
-  - Added the required column `homePage` to the `Image` table without a default value. This is not possible if the table is not empty.
-
-*/
 -- AlterTable
-ALTER TABLE "Image" ADD COLUMN     "homePage" BOOLEAN NOT NULL;
+ALTER TABLE "Image" ADD COLUMN     "homePage" BOOLEAN NOT NULL DEFAULT true;
--- prismaImages/migrations/20260511180947_home_page_default_true/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Image" ALTER COLUMN "homePage" SET DEFAULT true;
