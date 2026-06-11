7e644248553bce66013d4458cb53a9a4c2c64e15
CarterRoll <carter.rollins@noaa.gov>
2026-06-11 16:55:18 -0400

    Moves id to actually show graphs

--- app/(dashboard)/(content)/visualize/layout.tsx
+++ app/(dashboard)/(content)/visualize/layout.tsx
@@ -4,10 +4,12 @@ import { ReactNode } from "react";
 
 export default function VisualizeLayout({ children }: { children: ReactNode }) {
 	return (
-		<div id="search-results" className="flex flex-col gap-2 pt-6">
+		<div className="flex flex-col gap-2 pt-6">
 			<SearchUI noTable />
-			<VisualizeTabs />
-			{children}
+			<div id="search-results flex flex-col gap-2">
+				<VisualizeTabs />
+				{children}
+			</div>
 		</div>
 	);
 }
