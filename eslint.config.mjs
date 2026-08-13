cd8b4d9a35b582585baf07b87863a3446aec0d4f
CarterRoll <carter.rollins@noaa.gov>
2026-08-12 20:57:27 -0400

    Uses globalIgnores

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,22 @@
 /** @type { import("eslint").Linter.Config[] } */
 import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
 import nextTypescript from "eslint-config-next/typescript";
+import { globalIgnores } from "eslint/config";
 
 const eslintConfig = [
+	globalIgnores([
+		"node_modules/**",
+		".next/**",
+		"out/**",
+		"build/**",
+		"next-env.d.ts",
+		"app/generated/**",
+		"prisma/generated/**",
+		"prismaImages/generated/**"
+	]),
 	...nextCoreWebVitals,
 	...nextTypescript,
 	{
-		ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts", "**/generated/**"],
 		rules: {
 			"@typescript-eslint/no-explicit-any": "off",
 			"react-hooks/exhaustive-deps": "off"
