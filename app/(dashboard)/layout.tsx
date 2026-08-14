3533e20ec50368f07cc5dde10c89a16d3aa26487
Bayden <bwillms9@gmail.com>
2026-08-14 16:35:45 -0400

    Fixes empty right side margin of phone screens for all pages across the website

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -4,7 +4,7 @@ import { ReactNode } from "react";
 
 export default function DashboardLayout({ children }: { children: ReactNode }) {
 	return (
-		<div className="flex flex-col min-h-screen">
+		<div className="flex min-h-screen min-w-0 flex-col overflow-x-clip">
 			<button type="button" id="unfocusButton" tabIndex={-1} aria-hidden className="w-0 h-0"></button>
 			<Header />
 			{children}
